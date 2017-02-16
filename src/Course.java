437ebd307f6b99a3cdb541f72638f74d0af09efc
ChunTong <ctong48@uwo.ca>
2017-02-16 00:39:47 -0500

    Added Course

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by MasterChun on 17/2/16.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname=cname;
+        this.description=description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -3,12 +3,16 @@
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
     }
 
+    public void addCourse(Course c){
+        this.courselist.add(c);
+    }
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
     }
